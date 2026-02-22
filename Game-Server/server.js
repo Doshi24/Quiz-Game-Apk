f03a91f652e12c8ff672bb8dbb084f4ffb646954
MonilDoshi <doshimonil91@gamil.com>
2026-02-22 13:46:37 +0530

    Add server setup and memory store for game sessions

---
+++ Game-Server/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import http from "http"
+import { Server } from "socket.io"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const server = http.createServer(app)
+
+const io = new Server(server,{
+    cors:{
+        origin : "http://localhost:5173"
+    }
+})
+
+app.get('/testapi',(req,res)=>{
+    res.send("quiz Game Server is ready")
+})
+
+const PORT = 3000
+server.listen(PORT,()=>{
+    console.log(`server running on port ${PORT} `)
+})
---
+++ Game-Server/store/memoryStore.js
@@ -0,0 +1,31 @@
+
+export const watingplayes = {
+    easy : [],
+    medium : [],
+    hard : []
+}
+
+export const activeGames = new Map()
+
+export const addPlayersToQueues = (level,player)=>{
+    watingplayes[level].push(player)
+}
+
+export const RemovePlayersFromQueues = (level)=>{
+    watingplayes[level].shift()
+}
+
+
+export const CreateGameSession = (gameId,gameData)=>{
+    activeGames.set(gameId,gameData)
+}
+
+
+export const StoreGameSession = (gameId)=>{
+    activeGames.get(gameId)
+}
+
+
+export const RemoveGameSession = (gameId)=>{
+    activeGames.delete(gameId)
+}
